d62f17a7a8b2371ddc1b92d291ce3505266bdf07
Brit Butler <redline6561@gmail.com>
2012-06-28 16:47:49 -0400

    Add st-json writers for 6502 cpu and ram and init/exit stubs.

--- src/6502.lisp
+++ src/6502.lisp
@@ -1,5 +1,27 @@
 (in-package #:qeng-ho)
 
+(defmethod st-json:write-json-element ((element simple-array) stream)
+  (st-json:write-json-element (st-json:jso "6502-ram" (coerce element 'list))
+                              stream))
+
+(defmethod st-json:write-json-element ((element 6502-cpu:cpu) stream)
+  (let ((cpu-jso (st-json:jso "PC" (6502-cpu:cpu-pc element)
+                              "SP" (6502-cpu:cpu-sp element)
+                              "SR" (6502-cpu:cpu-sr element)
+                              "AR" (6502-cpu:cpu-ar element)
+                              "XR" (6502-cpu:cpu-xr element)
+                              "YR" (6502-cpu:cpu-yr element)
+                              "CC" (6502-cpu:cpu-cc element))))
+    (st-json:write-json-element (st-json:jso "6502-cpu" cpu-jso) stream)))
+
+(restas:define-route 6502/init
+    ("" :method :post :content-type "application/json")
+  nil)
+
+(restas:define-route 6502/exit
+    ("" :method :post :content-type "application/json")
+  nil)
+
 (restas:define-route 6502/reset
     ("" :method :post :content-type "application/json")
   nil)
